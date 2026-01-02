2d53033805ed2254ae68ebaa3c3ecc17cddaf771
Isaac249 <82252209+Isaac249@users.noreply.github.com>
2026-01-02 09:13:48 +0700

    chore: intial c sharp project based learning set up (calculator)

---
+++ CalculationHistory.cs
@@ -0,0 +1,17 @@
+//namespace HelloWorld
+//{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int a = 5;
+        int b = 10;
+        a=-3;
+        b = a;
+
+           Console.WriteLine(b);
+           Console.ReadKey();
+        Environment.Exit(0);
+        }
+    }
+//}
